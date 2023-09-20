c2729025706317642b4f11155a0260be6e32d057
Argos98p <jaio1998619@gmail.com>
2023-09-20 11:07:37 -0500

    create open youtube video

--- src/components/popup/AddYoutubeVideo.js
+++ src/components/popup/AddYoutubeVideo.js
@@ -9,6 +9,11 @@ const AddYoutubeVideo= ({onHandleInputYoutube})=>{
         <>
                 <input type="text" onChange={(e)=>onHandleInputYoutube(e.target.value)} placeholder={t("youtube_popup.link_youtube_text")} name="video-youtube"/>
 
+            <div className={"container-goto-youtube-button"}>
+                <div className={"goto-youtube-button"} onClick={()=>window.open("https://www.youtube.com/", '_blank')}> <p>{t("youtube_popup.to_youtube")}</p> </div>
+
+            </div>
+
         </>
     );
 }
