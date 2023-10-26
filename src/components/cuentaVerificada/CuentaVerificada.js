ba3a3d63be2cbaa78d40d7228a53d015962d3164
argos98p <jaio1998619@gmail.com>
2023-10-26 11:37:08 -0500

    update actived account

--- src/components/cuentaVerificada/CuentaVerificada.js
+++ src/components/cuentaVerificada/CuentaVerificada.js
@@ -2,14 +2,25 @@ import "./estilo.css"
 const CuentaVerificada = () => {
     return (
         <>
-            <div className={"header2"}>
-                <img className={"img2"} src={"/logo.png"} alt="logo "/>
+            <div className={'container-fluid cuenta-verificada  '}>
+                <div className={'container rounded-4'}>
+                    <div className={" rounded-4 header2 d-flex align-items-center justify-content-center flex-column"}>
+                        <a href={'http://info.3dmotores.com'} >
+                            <img className={"img2"} src={"/logo.png"} alt="logo "/>
+                        </a>
+
+                    </div>
+                    <div>
+
+                        <p className={"text-center2"}>¡Tu cuenta está verificada! Ahora, te guiaremos a través de cómo aprovechar al máximo la aplicación 3D MOTOR's. Podrás grabar, añadir hotspots y publicar tus creaciones en nuestro e-commerce de manera sencilla y efectiva.</p>
+                        <p className={"text-center2"}><a href={"https://www.youtube.com/watch?v=ouxXyP89rcc"}>Ver video tutorial</a> </p>
+                        <p className={"cerrar-ventana"}>Puedes cerrar esta ventana</p>
+
+                    </div>
+                </div>
             </div>
-            <div>
-                <h1 className={"text-center2"}>Tu Cuenta está verificada</h1>
-                <p className={"text-center2"}>Aquí te mostramos como funciona la aplicación 3D MOTOR's para que puedas sacarle el máximo provecho</p>
-                <p className={"text-center2"}><a href={"https://www.youtube.com/watch?v=ouxXyP89rcc"}>Ir al link de Youtube</a> </p>
-            </div>
+
+
         </>
     );
 }
